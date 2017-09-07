0d98e940d72a8b6c618cf43219a2b8daa40181fd
vagrant <vagrant@homestead>
2017-09-07 02:35:46 +0000

    update database configuration

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title', 'Sample App') - Laravel 5.5</title>
-  <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-@include('layouts._header')
-
-  <div class="container">
-    <div class="col-md-offset-1 col-md 10">
-      @include(('shared._messages'))
-      @yield('content')
-      @include('layouts._footer')
-    </div>
-  </div>
-</body>
+    <head>
+        <title>@yield('title', 'Sample App') - Laravel 5.5</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+    @include('layouts._header')
+        <div class="container">
+            <div class="col-md-offset-1 col-md 10">
+                @include(('shared._messages'))
+                @yield('content')
+                @include('layouts._footer')
+            </div>
+        </div>
+    </body>
 </html>
